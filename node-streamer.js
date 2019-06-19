18b1cfc464b8dfa12efdefd2b8872c93b3fc939f
Justin Nelson <jrnelson90@gmail.com>
2019-06-19 02:20:43 +0100

    Added output of IPv4 addresses to beginning of program

--- node-streamer.js
+++ node-streamer.js
@@ -1,6 +1,29 @@
 const cardInfo = require('node-alsa-cardinfo');
 const Arecord = require('node-arecord');
 const ffmpeg = require('fluent-ffmpeg');
+const os = require('os');
+
+const ifaces = os.networkInterfaces();
+
+Object.keys(ifaces).forEach(function (ifname) {
+  let alias = 0;
+
+  ifaces[ifname].forEach(function (iface) {
+    if ('IPv4' !== iface.family || iface.internal !== false) {
+      // skip over internal (i.e. 127.0.0.1) and non-ipv4 addresses
+      return;
+    }
+
+    if (alias >= 1) {
+      // this single interface has multiple ipv4 addresses
+      console.log(ifname + ':' + alias, iface.address);
+    } else {
+      // this interface has only one ipv4 adress
+      console.log(ifname, iface.address);
+    }
+    ++alias;
+  });
+});
 
 const cardName = "hw:CARD=audioinjectorpi,DEV=0";
 const playInfo = cardInfo.get(cardName, cardInfo.PLAYBACK);
