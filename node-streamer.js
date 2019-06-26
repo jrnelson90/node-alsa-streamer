ed1467c11f21a9f2dd5213eb0866697e9e5bcd73
Justin Nelson <jrnelson90@gmail.com>
2019-06-25 18:07:45 -0600

    reading sound card names

--- node-streamer.js
+++ node-streamer.js
@@ -3,6 +3,7 @@ const Arecord = require('node-arecord');
 const ffmpeg = require('fluent-ffmpeg');
 const os = require('os');
 const fs = require('fs');
+const cp = require("child_process");
 
 const ifaces = os.networkInterfaces();
 
@@ -37,6 +38,19 @@ try {
     });
 
     hwInfo = Object.create(null);
+
+    let cards;
+    cp.exec("arecord -L | grep :CARD", function (err, stdout, stderr) {
+      if(!stderr) {
+        cards = stdout;
+        console.info(cards);
+      } else {
+        console.error(stderr);
+        process.exit(1);
+      }
+      // If an error occurred, err will contain that error object
+      // The output for the command itself is held in stdout and stderr vars
+    });
 
     hwInfo["addresses"] = [...addrMap.values()];
     hwInfo["playback"] = cardInfo.get(cardName, cardInfo.PLAYBACK);
